5888fecf87ba3d732d4cdb73f441e0f6517b3990
Дмитрий <50398746+Dmitriy101@users.noreply.github.com>
2021-05-18 10:04:39 +0800

    1.13. Работа с условиями (правки)

--- index.php
+++ index.php
@@ -72,8 +72,7 @@ $show_complete_tasks = rand(0, 1);
 
                     <label class="checkbox">
                         <!--добавить сюда атрибут "checked", если переменная $show_complete_tasks равна единице-->
-                        <input class="checkbox__input visually-hidden show_completed" type="checkbox"
-                            <? if ($show_complete_tasks == 1): ?> checked <? endif; ?>>
+                        <input class="checkbox__input visually-hidden show_completed" type="checkbox" <?php if ($show_complete_tasks == 1): ?>checked<?php endif; ?>>
                         <span class="checkbox__text">Показывать выполненные</span>
                     </label>
                 </div>
@@ -93,8 +92,8 @@ $show_complete_tasks = rand(0, 1);
 
                         <td class="task__date"></td>
                     </tr>
-                    <? if ($show_complete_tasks == 1): ?>
-                        <tr class="tasks__item task <? if ($show_complete_tasks == 1): ?> task--completed <? endif; ?>">
+                    <?php if ($show_complete_tasks == 1): ?>
+                        <tr class="tasks__item task task--completed">
                             <td class="task__select">
                                 <label class="checkbox task__checkbox">
                                     <input class="checkbox__input visually-hidden" type="checkbox" checked>
@@ -106,7 +105,7 @@ $show_complete_tasks = rand(0, 1);
                             <td class="task__controls">
                             </td>
                         </tr>
-                    <? endif; ?>
+                    <?php endif; ?>
                     <!--показывать следующий тег <tr/>, если переменная $show_complete_tasks равна единице-->
                 </table>
             </main>
