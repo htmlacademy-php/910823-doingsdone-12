ed5f517dd1ca28e15ec11cfca076b6cf6386516a
Дмитрий <50398746+Dmitriy101@users.noreply.github.com>
2021-07-17 14:17:04 +0800

    Работаем с датой

--- index.php
+++ index.php
@@ -2,12 +2,15 @@
 include_once('helpers.php');
 include_once('my_function.php');
 
+// показывать или нет выполненные задачи
+$show_complete_tasks = rand(0, 1);
+
 // массив проектов и задач
 $projects = ['Входящие', 'Учеба', 'Работа', 'Домашние дела', 'Авто'];
 $tasks = [
     [
         'name' => 'Собеседование в IT компании',
-        'date' => '01.12.2019',
+        'date' => '16.07.2022',
         'category' => 'Работа',
         'result' => false,
     ],
@@ -25,7 +28,7 @@ $tasks = [
     ],
     [
         'name' => 'Встреча с другом',
-        'date' => '22.12.2019',
+        'date' => '16.07.2021',
         'category' => 'Входящие',
         'result' => false,
     ],
@@ -44,7 +47,7 @@ $tasks = [
 ];
 
 // HTML код главной страницы 
-$page_content = include_template('main.php', ['projects' => $projects, 'tasks' => $tasks]);
+$page_content = include_template('main.php', ['projects' => $projects, 'tasks' => $tasks, 'show_complete_tasks' => $show_complete_tasks]);
 
 // окончательный HTML код 
 $layout_content = include_template('layout.php', ['content' => $page_content, 'title_tab' => 'Дела в порядке']);
--- my_function.php
+++ my_function.php
@@ -18,3 +18,26 @@ function calc_for_project($array_tasks, $project_name) {
     return $result;
 }
 
+/**
+ * Подсчет разницы (в часах) между двумя датами
+ * @param array $array_date Массив дат из задач
+ * @param integer $current_date Текущая дата
+ * @param string $task_date Дата из задачи
+ * @param integer $calc_date Получаем разницу дат $current_date и $task_date
+ * @param integer $result Привод к целому числу и перевод в часы
+ * @return integer Результат
+ */
+function show_date($array_date) {
+
+     $current_date = strtotime("now");
+     $task_date = strtotime($array_date);
+     $calc_date = $current_date - $task_date;
+
+     if ($array_date == null) {
+         return false;
+     }
+
+     $result = floor($calc_date/3600);
+
+    return $result;
+}
